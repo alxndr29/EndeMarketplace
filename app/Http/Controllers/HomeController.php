3c60b4c13dd8251c5b1cd715a5cd83f4522a7f76
alxndr29 <61173884+alxndr29@users.noreply.github.com>
2021-03-14 23:14:48 +0800

    Integrasi AdminLTE

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,11 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
+
+    /*public function __construct()
     {
         $this->middleware(['auth', 'verified']);
-    }
+    }*/
 
     /**
      * Show the application dashboard.
@@ -23,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('seller.produk.index');
     }
 }
